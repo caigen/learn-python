0b134e6f701826fc8df66c7db756491ba4aabe34
caigen <caigen@outlook.com>
2019-11-27 11:06:32 +0800

    gil test.

---
+++ gil/single_threaded.py
@@ -0,0 +1,19 @@
+"""
+ref: https://realpython.com/python-gil/
+"""
+import time
+from threading import Thread
+
+COUNT = 50000000
+
+
+def countdown(n):
+    while n > 0:
+        n -= 1
+
+
+start = time.time()
+countdown(COUNT)
+end = time.time()
+
+print('Time taken in seconds: ', end - start)
