687c13115e32c2e7b434bbd961a9f0e1f146540b
caigen <caigen@outlook.com>
2019-12-09 17:10:59 +0800

    pandas change columns/save/load

--- pandas/hello-pandas.py
+++ pandas/hello-pandas.py
@@ -24,6 +24,13 @@ print(df1)
 print(df2)
 print(df0)
 
+# Add Column
+print("")
+print("Add Columns:")
+df0["second_column"] = 0
+df0["third_column"] = "empty"
+print(df0)
+
 # Query
 data_two_column = {"a": [1, 1, 2], "b": ["hello", "world", "world"]}
 df_two_column = pd.DataFrame(data_two_column)
@@ -31,3 +38,13 @@ print(df_two_column)
 print(df_two_column[df_two_column.a == 1])
 print(df_two_column[df_two_column.b == "world"])
 print(df_two_column.query("a == 1 & b =='world'"))
+
+# Save & Load
+print("")
+print("Save & Load:")
+df_two_column.to_csv("data.csv", index_label="id")
+df_loaded = pd.read_csv("data.csv", index_col="id")
+print(df_loaded)
+print(df_loaded[df_two_column.a == 1])
+print(df_loaded[df_two_column.b == "world"])
+print(df_loaded.query("a == 1 & b =='world'"))
