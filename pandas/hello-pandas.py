972314cf673aaae1a12f324aa2b05bf0f7f20cee
caigen <caigen@outlook.com>
2019-12-09 15:19:54 +0800

    pandas query

--- pandas/hello-pandas.py
+++ pandas/hello-pandas.py
@@ -1,7 +1,7 @@
 import pandas as pd
 
 # Column name
-first_column = "first column"
+first_column = "first_column"
 
 # No data
 data0 = {first_column: []}
@@ -23,3 +23,11 @@ df0 = df1.append(df2, ignore_index=True)
 print(df1)
 print(df2)
 print(df0)
+
+# Query
+data_two_column = {"a": [1, 1, 2], "b": ["hello", "world", "world"]}
+df_two_column = pd.DataFrame(data_two_column)
+print(df_two_column)
+print(df_two_column[df_two_column.a == 1])
+print(df_two_column[df_two_column.b == "world"])
+print(df_two_column.query("a == 1 & b =='world'"))
